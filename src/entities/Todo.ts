22e76be63f14efafac9cbf7a37ebacdd959d1c0b
devkeita <keita.mywork@gmail.com>
2021-02-12 21:05:42 +0900

    Todoのレイアウト作成

---
+++ src/entities/Todo.ts
@@ -0,0 +1,5 @@
+export type TTodo = {
+    Title: string
+    Description: string
+    Status: boolean
+}
--- src/pages/todo.tsx
+++ src/pages/todo.tsx
@@ -3,55 +3,53 @@ import Container from "@material-ui/core/Container";
 import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
 import ListItemText from "@material-ui/core/ListItemText";
-import ListItemAvatar from "@material-ui/core/ListItemAvatar";
-import Avatar from "@material-ui/core/Avatar";
-import Typography from "@material-ui/core/Typography";
-import { useFamily } from "../data/use-family";
-import { useMe } from "../data/use-me";
-import { TUser } from "../entities/User";
+import {TTodo} from "../entities/Todo";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import Checkbox from "@material-ui/core/Checkbox";
+import ListItemSecondaryAction from "@material-ui/core/ListItemSecondaryAction";
+import IconButton from "@material-ui/core/IconButton";
+import CommentIcon from '@material-ui/icons/Comment';
 
 const Index: React.FC = () => {
-    const [users, setUsers] = useState<TUser[]>([]);
-
-    const {family, error} = useFamily();
-    const {user} = useMe();
+    const [todos, setTodos] = useState<TTodo[]>([]);
 
     useEffect(() => {
-        if (family && user) {
-            const users = family.Users.filter(value => {
-                return value.ID != user.ID
-            });
-            setUsers([...users, user]);
-        }
-    }, [family, user])
-
-    if (error) return <div>failed to load</div>
-    if (!family && !error) return null;
+        const todo: TTodo[] = [
+            {
+                Title: "犬の散歩",
+                Description: "犬の散歩に行く",
+                Status: true,
+            },
+            {
+                Title: "犬のご飯",
+                Description: "犬にご飯を上げる",
+                Status: false,
+            },
+        ];
+        setTodos(todo);
+    }, [])
 
     return (
         <React.Fragment>
             <Container component={"main"} maxWidth={"xs"}>
                 <List>
-                    {users.map((value, index) => {
+                    {todos.map((value, index) => {
                         return (
-                            <ListItem key={index} alignItems="flex-start">
-                                <ListItemAvatar>
-                                    <Avatar alt="アカウントのアイコン" src={value.PictureUrl}/>
-                                </ListItemAvatar>
+                            <ListItem key={index}>
+                                <ListItemIcon>
+                                    <Checkbox
+                                        edge="start"
+                                        checked={value.Status}
+                                    />
+                                </ListItemIcon>
                                 <ListItemText
-                                    primary={value.Name}
-                                    secondary={
-                                        <React.Fragment>
-                                            <Typography
-                                                component="span"
-                                                variant="body2"
-                                                color="textPrimary"
-                                            >
-                                                {value.Status}
-                                            </Typography>
-                                        </React.Fragment>
-                                    }
+                                    primary={value.Title}
                                 />
+                                <ListItemSecondaryAction>
+                                    <IconButton edge="end" aria-label="comments">
+                                        <CommentIcon />
+                                    </IconButton>
+                                </ListItemSecondaryAction>
                             </ListItem>
                         )
                     })}
