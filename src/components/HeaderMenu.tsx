4f88bfa5da13a6908712ce5a6cc9268d32ae8e76
devkeita <keita.mywork@gmail.com>
2021-02-12 22:31:44 +0900

    family leaveを実装

--- src/components/HeaderMenu.tsx
+++ src/components/HeaderMenu.tsx
@@ -1,16 +1,16 @@
 import React, {useState} from "react";
-import FamilyInvitationCodeMenu from "./FamilyInvitationCodeMenu";
-import UserStatusFormMenu from "./UserStatusFormMenu";
 import Menu from "@material-ui/core/Menu";
 import MenuItem from "@material-ui/core/MenuItem";
 import FamilyInvitationCodeDialog from "./FamilyInvitationCodeDialog";
 import UserStatusForm from "./UserStatusForm";
+import FamilyLeaveDialog from "./FamilyLeaveDialog";
 
 const HeaderMenu = ({ anchorEl, setAnchorEl }) => {
     const open = Boolean(anchorEl);
 
     const [firstMenu, setFirstMenu] = useState<boolean>(false);
     const [secondMenu, setSecondMenu] = useState<boolean>(false);
+    const [thirdMenu, setThirdMenu] = useState<boolean>(false);
 
     const handleClose = () => {
         setAnchorEl(null);
@@ -33,8 +33,10 @@ const HeaderMenu = ({ anchorEl, setAnchorEl }) => {
         >
             <MenuItem onClick={() => setFirstMenu(true)}>招待コードを表示</MenuItem>
             <MenuItem onClick={() => setSecondMenu(true)}>ステータス変更</MenuItem>
+            <MenuItem onClick={() => setThirdMenu(true)}>ファミリーを抜ける</MenuItem>
             <FamilyInvitationCodeDialog open={firstMenu} setOpen={setFirstMenu} />
             <UserStatusForm open={secondMenu} setOpen={setSecondMenu} />
+            <FamilyLeaveDialog open={thirdMenu} setOpen={setThirdMenu} />
         </Menu>
     )
 }
